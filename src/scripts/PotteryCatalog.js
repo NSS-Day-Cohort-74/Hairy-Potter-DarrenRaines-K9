9356ab504e862e5e7104e76238e64b4c5024047c
Darren Raines <draines.123123@gmail.com>
2024-12-10 16:23:11 -0600

    no message just verification

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,10 @@
+//Here I created a variable named madePottery to equal an empty array
+let madePottery = []
+//Here I am creating a function toSellOrNotToSell() whose responsibilty is return a augmented array of objects with a new property of "price"
+export const toSellOrNotToSell = (potteryObject, potteryWeight) => {
+   
+    
+    for (const element of object) {
+        
+    }
+}
